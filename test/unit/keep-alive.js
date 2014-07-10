073dd687e22bcc2656a4149b5524330de870c54d
Charles Hill <chill@degreesofzero.com>
2014-07-10 11:56:33 +0200

    Added tests for keep-alive signals while database connection is disconnected and after the connection has been re-established.

--- test/unit/keep-alive.js
+++ test/unit/keep-alive.js
@@ -299,4 +299,159 @@ describe('MySQLConnectionManager#', function() {
 
 	})
 
+	describe('keep-alive', function() {
+
+		describe('While the database connection is disconnected', function() {
+
+			var options = {
+				host: config.host,
+				port: config.port,
+				user: config.user,
+				password: config.password,
+				database: config.database,
+				autoReconnect: true,
+				reconnectDelay: [ 5000 ],
+				useConnectionPooling: false,
+				reconnectDelayGroupSize: 3,
+				maxReconnectAttempts: 5,
+				keepAlive: true,
+				keepAliveInterval: 15
+			}
+
+			var manager
+
+			before(function(done) {
+
+				manager = new MySQLConnectionManager(options)
+
+				manager.once('connect', function() {
+
+					done()
+
+				})
+
+			})
+
+			before(function(done) {
+
+				manager.once('disconnect', function() {
+
+					done()
+
+				})
+
+				manager.connection.destroy()
+				manager.connection.emit('error', {code: 'PROTOCOL_CONNECTION_LOST'})
+
+			})
+
+			after(function() {
+
+				if (manager.connection.state != 'disconnected')
+					manager.connection.destroy()
+
+			})
+
+			it('should not send keep-alive signals', function(done) {
+
+				var numCalls = 0
+
+				// Override the keepAlive method.
+				manager.keepAlive = function() {
+
+					numCalls++
+
+				}
+
+				setTimeout(function() {
+
+					expect(numCalls).to.equal(0)
+					done()
+
+				}, 55)
+				
+			})
+
+		})
+
+		describe('After the database connection has been re-established', function() {
+
+			var options = {
+				host: config.host,
+				port: config.port,
+				user: config.user,
+				password: config.password,
+				database: config.database,
+				autoReconnect: true,
+				reconnectDelay: [ 15, 25 ],
+				useConnectionPooling: false,
+				reconnectDelayGroupSize: 3,
+				maxReconnectAttempts: 5,
+				keepAlive: true,
+				keepAliveInterval: 15
+			}
+
+			var manager
+
+			before(function(done) {
+
+				manager = new MySQLConnectionManager(options)
+
+				manager.once('connect', function() {
+
+					done()
+
+				})
+
+			})
+
+			before(function(done) {
+
+				manager.once('reconnect', function() {
+
+					done()
+
+				})
+
+				manager.connection.destroy()
+				manager.connection.emit('error', {code: 'PROTOCOL_CONNECTION_LOST'})
+
+			})
+
+			after(function() {
+
+				if (manager.connection.state != 'disconnected')
+					manager.connection.destroy()
+
+			})
+
+			it('should send keep-alive signals', function(done) {
+
+				var timeout, called = false
+
+				// Override the keepAlive method.
+				manager.keepAlive = function() {
+
+					clearTimeout(timeout)
+
+					if (!called)
+					{
+						called = true
+						done()
+					}
+
+				}
+
+				timeout = setTimeout(function() {
+
+					done(new Error('Expected at least one keep-alive signal to have been sent.'))
+
+				}, 60)
+
+			})
+
+		})
+
+	})
+
 })
