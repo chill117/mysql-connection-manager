0498c991b0db94258f2dab80f755f882db7a8b6f
Charles Hill <chill@degreesofzero.com>
2014-07-10 11:45:31 +0200

    Tests for 'keepAlive' option.

--- test/unit/keep-alive.js
+++ test/unit/keep-alive.js
@@ -6,7 +6,126 @@ var config = require('../config/database')
 
 describe('MySQLConnectionManager#', function() {
 
-	describe('options: keepAliveInterval', function() {
+	describe('option: \'keepAlive\'', function() {
+
+		describe('is set to FALSE', function() {
+
+			var options = {
+				host: config.host,
+				port: config.port,
+				user: config.user,
+				password: config.password,
+				database: config.database,
+				keepAlive: false,
+				keepAliveInterval: 15
+			}
+
+			var manager
+
+			before(function(done) {
+
+				manager = new MySQLConnectionManager(options)
+
+				manager.once('connect', function() {
+
+					done()
+
+				})
+
+			})
+
+			after(function() {
+
+				if (manager.connection.state != 'disconnected')
+					manager.connection.destroy()
+
+			})
+
+			it('should not send keep-alive signals', function(done) {
+
+				var numCalls = 0
+
+				// Override the keepAlive method.
+				manager.keepAlive = function() {
+
+					numCalls++
+
+				}
+
+				setTimeout(function() {
+
+					expect(numCalls).to.equal(0)
+					done()
+
+				}, 55)
+
+			})
+
+		})
+
+		describe('when set to TRUE', function() {
+
+			var options = {
+				host: config.host,
+				port: config.port,
+				user: config.user,
+				password: config.password,
+				database: config.database,
+				keepAlive: true,
+				keepAliveInterval: 15
+			}
+
+			var manager
+
+			before(function(done) {
+
+				manager = new MySQLConnectionManager(options)
+
+				manager.once('connect', function() {
+
+					done()
+
+				})
+
+			})
+
+			after(function() {
+
+				if (manager.connection.state != 'disconnected')
+					manager.connection.destroy()
+
+			})
+
+			it('should send keep-alive signals', function(done) {
+
+				var timeout, called = false
+
+				// Override the keepAlive method.
+				manager.keepAlive = function() {
+
+					clearTimeout(timeout)
+
+					if (!called)
+					{
+						called = true
+						done()
+					}
+
+				}
+
+				timeout = setTimeout(function() {
+
+					done(new Error('Expected at least one keep-alive signal to have been sent.'))
+
+				}, 60)
+
+			})
+			
+		})
+
+	})
+
+	describe('option: \'keepAliveInterval\'', function() {
 
 		var options = {
 			host: config.host,
